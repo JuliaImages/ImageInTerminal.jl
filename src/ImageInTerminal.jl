1f0a574629e9083fa9a0c2691934527e1397fbbd
Johnny Chen <johnnychen94@hotmail.com>
2020-07-04 16:21:36 +0800

    provide an enable/disable interfact
    This commit adds two function: `enable_encoding` and `disable_encoding`
    so that user can manually switch between on/off mode.

--- src/ImageInTerminal.jl
+++ src/ImageInTerminal.jl
@@ -19,6 +19,7 @@ include("imshow.jl")
 # overload default show in the REPL for colorant (arrays)
 
 const colormode = TermColorDepth[TermColor256()]
+const should_render_image = Bool[true]
 
 """
     use_256()
@@ -26,7 +27,7 @@ const colormode = TermColorDepth[TermColor256()]
 Triggers `imshow256` automatically if an array of colorants is to
 be displayed in the julia REPL. (This is the default)
 """
-use_256() = (colormode[1] = TermColor256())
+use_256() = (colormode[1] = TermColor256(); should_render_image[1] = true)
 
 """
     use_24bit()
@@ -35,28 +36,57 @@ Triggers `imshow24bit` automatically if an array of colorants is to
 be displayed in the julia REPL.
 Call `ImageInTerminal.use_256()` to restore default behaviour.
 """
-use_24bit() = (colormode[1] = TermColor24bit())
+use_24bit() = (colormode[1] = TermColor24bit(); should_render_image[1] = true)
+
+"""
+    disable_encoding()
+
+Disable the image encoding feature and show images as if they are normal arrays.
+
+This can be restored by calling `ImageInTerminal.enable_encoding()`.
+"""
+disable_encoding() = (should_render_image[1] = false)
+
+"""
+    enable_encoding()
+
+Enable the image encoding feature and show images in terminal.
+
+This can be disabled by calling `ImageInTerminal.disable_encoding()`. To choose between
+different encoding method, call `ImageInTerminal.use_256()` or `ImageInTerminal.use_24bit()`.
+"""
+enable_encoding() = (should_render_image[1] = true)
+
 
 # colorant arrays
 function Base.show(
-        io::IO, ::MIME"text/plain",
+        io::IO, mime::MIME"text/plain",
         img::AbstractArray{<:Colorant})
-    println(io, summary(img), ":")
-    ImageInTerminal.imshow(io, img, colormode[1])
+    if should_render_image[1]    
+        println(io, summary(img), ":")
+        ImageInTerminal.imshow(io, img, colormode[1])
+    else
+        invoke(Base.show, Tuple{typeof(io), typeof(mime), AbstractArray}, io, mime, img)
+    end
 end
 
 # colorant
-function Base.show(io::IO, ::MIME"text/plain", color::Colorant)
-    fgcol = _colorant2ansi(color, colormode[1])
-    chr = _charof(alpha(color))
-    print(io, Crayon(foreground = fgcol), chr, chr, " ")
-    print(io, Crayon(foreground = :white), color)
-    print(io, Crayon(reset = true))
+function Base.show(io::IO, mime::MIME"text/plain", color::Colorant)
+    if should_render_image[1]
+        fgcol = _colorant2ansi(color, colormode[1])
+        chr = _charof(alpha(color))
+        print(io, Crayon(foreground = fgcol), chr, chr, " ")
+        print(io, Crayon(foreground = :white), color)
+        print(io, Crayon(reset = true))
+    else
+        invoke(Base.show, Tuple{typeof(io), typeof(mime), Any}, io, mime, color)
+    end
 end
 
 function __init__()
     # use 24bit if the terminal supports it
     lowercase(get(ENV, "COLORTERM", "")) in ("24bit", "truecolor") && use_24bit()
+    enable_encoding()
 end
 
 end # module
--- test/tst_baseshow.jl
+++ test/tst_baseshow.jl
@@ -1,4 +1,49 @@
-_tostring(io) = replace.(replace.(readlines(seek(io,0)), Ref("\n" => "")), Ref("$Int" => "Int64"))
+function _tostring(io)
+    contents = map(readlines(seek(io,0))) do line
+        replace(strip(line), "$Int" => "Int64")
+    end
+end
+
+@testset "enable/disable encoding" begin
+    old_colormode = ImageInTerminal.colormode[1]
+    old_should_render_image = ImageInTerminal.should_render_image[1]
+
+    ImageInTerminal.enable_encoding()
+    @test ImageInTerminal.colormode[1] == old_colormode
+    @test ImageInTerminal.should_render_image[1] == true
+
+    ImageInTerminal.enable_encoding()
+    ImageInTerminal.disable_encoding()
+    @test ImageInTerminal.colormode[1] == old_colormode
+    @test ImageInTerminal.should_render_image[1] == false
+
+    ImageInTerminal.disable_encoding()
+    ImageInTerminal.use_256()
+    @test ImageInTerminal.colormode[1] == ImageInTerminal.TermColor256()
+    @test ImageInTerminal.should_render_image[1] == true
+
+    ImageInTerminal.disable_encoding()
+    ImageInTerminal.use_24bit()
+    @test ImageInTerminal.colormode[1] == ImageInTerminal.TermColor24bit()
+    @test ImageInTerminal.should_render_image[1] == true
+
+    ImageInTerminal.colormode[1] = old_colormode
+    ImageInTerminal.should_render_image[1] = old_should_render_image
+end
+
+@testset "no encoding" begin
+    ImageInTerminal.disable_encoding()
+    io = IOBuffer()
+    img = fill(RGB(1.0, 1.0, 1.0), 4, 4)
+    show(io, MIME"text/plain"(), img)
+    @test_reference "reference/2d_show_raw.txt" _tostring(io)
+    io = IOBuffer()
+    show(io, MIME"text/plain"(), collect(rgb_line))
+    @test_reference "reference/rgbline_show_raw.txt" _tostring(io)
+    io = IOBuffer()
+    show(io, MIME"text/plain"(), RGB(0.5,0.1,0.9))
+    @test_reference "reference/colorant_show_raw.txt" _tostring(io)
+end
 
 @testset "256 colors" begin
     ImageInTerminal.use_256()
