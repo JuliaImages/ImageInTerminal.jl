6a547bc0af04fc6237a9bdf6a07b873a339f5829
t-bltg <tf.bltg@gmail.com>
2022-09-20 17:15:50 +0200

    add callback kw

--- src/ImageInTerminal.jl
+++ src/ImageInTerminal.jl
@@ -107,24 +107,24 @@ Supported encoding:
     - ascii (`XTermColors` backend)
 """
 
-function imshow(io::IO, img::AbstractArray{<:Colorant}, maxsize::Tuple=displaysize(io))
+function imshow(io::IO, img::AbstractArray{<:Colorant}, maxsize::Tuple=displaysize(io); callback = nothing)
     buf = IOContext(PipeBuffer(), :color => get(io, :color, false))
     if choose_sixel(img)
         sixel_encode(buf, img)
     else
         if ndims(img) > 2
             Base.show_nd(
-                buf, img, (buf, x) -> ascii_show(buf, x, COLORMODE[], :auto, maxsize), true
+                buf, img, (buf, x) -> ascii_show(buf, x, COLORMODE[], :auto, maxsize; callback = callback), true
             )
         else
-            ascii_show(buf, img, COLORMODE[], :auto, maxsize)
+            ascii_show(buf, img, COLORMODE[], :auto, maxsize; callback = callback)
         end
     end
     write(io, read(buf, String))
 end
 
-imshow(img::AbstractArray{<:Colorant}, args...) = imshow(stdout, img, args...)
-imshow(img, args...) =
+imshow(img::AbstractArray{<:Colorant}, args...; kw...) = imshow(stdout, img, args...; kw...)
+imshow(img, args...; kw...) =
     throw(ArgumentError("imshow only supports colorant arrays with 1 or 2 dimensions"))
 
 sixel_encode(args...; kwargs...) = Sixel.sixel_encode(args...; kwargs...)
